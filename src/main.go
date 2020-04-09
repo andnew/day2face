34d54b0737ff5bafff52170a07ef23d6a170c404
yong.wang <yong.wang@ucarinc.com>
2020-04-09 23:07:35 +0800

    第76天 Go 面试题 结构体嵌套 方法覆盖

--- src/main.go
+++ src/main.go
@@ -1,22 +1,55 @@
 package main
 
-import (
-	"fmt"
-	"testing"
-)
+import "fmt"
 
-func hello(num ...int) {
-	num[0] = 18
+type S1 struct{}
+
+func (s1 S1) f() {
+	fmt.Println("S1.f()")
 }
-func Test13(t *testing.T) {
-	i := []int{5, 6, 7}
-	hello(i...)
-	fmt.Println(i[0])
+func (s1 S1) g() {
+	fmt.Println("S1.g()")
 }
+
+type S2 struct {
+	S1
+}
+
+func (s2 S2) f() {
+	fmt.Println("S2.f()")
+}
+
+type I interface {
+	f()
+}
+
+func printType(i I) {
+
+	fmt.Printf("%T\n", i)
+	if s1, ok := i.(S1); ok {
+		s1.f()
+		s1.g()
+	}
+	if s2, ok := i.(S2); ok {
+		s2.f()
+		s2.g()
+	}
+}
+
 func main() {
-	t := &testing.T{}
-	Test13(t)
+	printType(S1{})
+	printType(S2{})
+
+	// 执行结果
+	//main.S1
+	//S1.f()
+	//S1.g()
+	//main.S2
+	//S2.f()
+	//S1.g()
+	//
+
 }
 
 // 总结&分析
-// 可变函数是指针传递
+// 知识点：类型断言，结构体嵌套。结构体 S2 嵌套了结构体 S1，S2 自己没有实现 g() ，调用的是 S1 的 g()。
