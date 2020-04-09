234390c97a05266979998df887da770130b8dac3
yong.wang <yong.wang@ucarinc.com>
2020-04-09 22:55:12 +0800

    第78天 Go 面试题 代码断行规则

--- src/main.go
+++ src/main.go
@@ -1,20 +1,16 @@
 package main
 
-import "fmt"
-
+func alwaysFalse() bool {
+	return false
+}
 func main() {
-	c := &ConfigOne{}
-	c.String()
-}
-
-type ConfigOne struct {
-	Daemon string
-}
-
-func (c *ConfigOne) String() string {
-	return fmt.Sprintf("print: %v", c)
+	switch alwaysFalse(); {
+	case true:
+		println(true)
+	case false:
+		println(false)
+	}
 }
 
 // 总结&分析
-// 知识点：Go语言的内存回收机制规定，只要有一个指针指向引用一个变量，那么这个变量就不会被释放（内存逃逸），
-// 因此在 Go 语言中返回函数参数或临时变量是安全的。
+// 可以编译通过，输出：true。知识点：Go 代码断行规则。
