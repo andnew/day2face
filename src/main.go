8a3e3eedfca8f7618268ff71a9d119a6e52e40bb
yong.wang <yong.wang@ucarinc.com>
2020-04-10 06:58:34 +0800

    第73天 Go 面试题 匿名函数和for循环

--- src/main.go
+++ src/main.go
@@ -1,38 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"runtime"
-)
+func test() []func() {
+	var funs []func()
+	for i := 0; i < 2; i++ {
+		j := i
+		funs = append(funs, func() {
+			println(&j, j)
+		})
+	}
+	return funs
+}
 
-//func main() {
-//	runtime.GOMAXPROCS(1)
-//
-//	go func() {
-//		for i := 0; i < 10; i++ {
-//			fmt.Println(i)
-//		}
-//	}()
-//
-//	for {
-//		fmt.Println("a")
-//	}
-//}
-
-//可以通过阻塞的方式避免 CPU 占用，修复代码：
 func main() {
-	runtime.GOMAXPROCS(1)
-
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(i)
-		}
-		os.Exit(0)
-	}()
-
-	select {}
+	funs := test()
+	for _, f := range funs {
+		f()
+	}
 }
 
 // 总结&分析
-// for {} 独占 CPU 资源导致其他 Goroutine 饿死。
+// 闭包延迟求值。for 循环局部变量 i，匿名函数每一次使用的都是同一个变量。
