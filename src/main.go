25f415eea205018ed2a6e97119662bae48f83c16
yong.wang <yong.wang@ucarinc.com>
2020-04-09 23:20:44 +0800

    第74天 Go 面试题 Goroutine

--- src/main.go
+++ src/main.go
@@ -1,20 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
-func f() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Printf("recover:%#v", r)
-		}
-	}()
-	panic(1)
-	panic(2)
-}
+//func main() {
+//	runtime.GOMAXPROCS(1)
+//
+//	go func() {
+//		for i := 0; i < 10; i++ {
+//			fmt.Println(i)
+//		}
+//	}()
+//
+//	for {
+//		fmt.Println("a")
+//	}
+//}
 
+//可以通过阻塞的方式避免 CPU 占用，修复代码：
 func main() {
-	f()
+	runtime.GOMAXPROCS(1)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+		os.Exit(0)
+	}()
+
+	select {}
 }
 
 // 总结&分析
-// 知识点：panic、recover()。当程序 panic 时就不会往下执行，可以使用 recover() 捕获 panic 的内容
+// for {} 独占 CPU 资源导致其他 Goroutine 饿死。
