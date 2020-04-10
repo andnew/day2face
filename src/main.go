b3e292e8cb060175207455ac223efc8d2d73556f
yong.wang <yong.wang@ucarinc.com>
2020-04-10 14:56:04 +0800

    第71天 Go 面试题 函数 defer

--- src/main.go
+++ src/main.go
@@ -12,35 +12,8 @@ func (s *Slice) Add(elem int) *Slice {
 	fmt.Print(elem)
 	return s
 }
-
-type Orange struct {
-	Quantity int
-}
-
-func (o *Orange) Increase(n int) {
-	o.Quantity += n
-}
-
-func (o *Orange) Decrease(n int) {
-	o.Quantity -= n
-}
-
-func (o *Orange) String() string {
-	return fmt.Sprintf("---%#v", o.Quantity)
-}
-
 func main() {
-	//s := NewSlice()
-	//defer func() {
-	//	s.Add(1).Add(2)
-	//}()
-	//s.Add(3)
-
-	var orange Orange
-	orange.Increase(10)
-	orange.Decrease(5)
-	fmt.Println(orange)
+	s := NewSlice()
+	defer s.Add(1).Add(2)
+	s.Add(3)
 }
-
-// 总结&分析
-// 闭包延迟求值。for 循环局部变量 i，匿名函数每一次使用的都是同一个变量。
