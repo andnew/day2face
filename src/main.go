ffeef2e7bed2fb5ad9ecc212af3dd648510b2781
yong.wang <yong.wang@ucarinc.com>
2020-04-11 14:22:55 +0800

    第61天 Go 面试题 WaitGroup

--- src/main.go
+++ src/main.go
@@ -1,36 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-//func main() {
-//	var k = 1
-//	var s = []int{1, 2}
-//	k, s[k] = 0, 3
-//	fmt.Println(s[0] + s[1])
+//type T struct{}
+//
+//func (*T) foo() {
 //}
-
+//func (T) bar() {
+//}
+//
+//type S struct {
+//	*T
+//}
+//
+//func main() {
+//	s := S{}
+//	fmt.Printf("%#v\n",s) //main.S{T:(*main.T)(nil)}
+//	_ = s.foo
+//	s.foo()
+//	_ = s.bar //s.bar 将被展开为 (*s.T).bar
+//}
 // 总结&分析
-// k, s[k] = 0, 3 ==> k, s[1] = 0, 3
+// panic: runtime error: invalid memory address or nil pointer dereference
 
-func main() {
-	var k = 9
-	for k = range []int{} {
-		fmt.Println("=====")
-	}
-	fmt.Println(k)
+//type data struct {
+//	sync.Mutex
+//}
+//
+//func (d data) test(s string) {// 修改为 func (d *data) test(s string) {
+//	d.Lock()
+//	defer d.Unlock()
+//
+//	for i := 0; i < 5; i++ {
+//		fmt.Println(s, i)
+//		time.Sleep(time.Second)
+//	}
+//}
+//
+//func main() {
+//
+//	var wg sync.WaitGroup
+//	wg.Add(2)
+//	var d data
+//
+//	go func() {
+//		defer wg.Done()
+//		d.test("read")
+//	}()
+//
+//	go func() {
+//		defer wg.Done()
+//		d.test("write")
+//	}()
+//
+//	wg.Wait()
+//}
+// 总结&分析
+// 锁失效。将 Mutex 作为匿名字段时，相关的方法必须使用指针接收者，否则会导致锁机制失效。
 
-	for k = 0; k < 3; k++ {
-		fmt.Println("----")
-	}
-	fmt.Println(k)
-
-	for k = range (*[3]int)(nil) {
-		fmt.Println("666--")
-	}
-	fmt.Println(k)
+type data struct {
+	*sync.Mutex // *Mutex
 }
 
-// 执行结果
-//9
-//3
-//2
+func (d data) test(s string) { // 值方法
+	d.Lock()
+	defer d.Unlock()
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(s, i)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	d := data{new(sync.Mutex)} // 初始化
+
+	go func() {
+		defer wg.Done()
+		d.test("read")
+	}()
+
+	go func() {
+		defer wg.Done()
+		d.test("write")
+	}()
+
+	wg.Wait()
+}
