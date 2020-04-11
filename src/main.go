575b3a5fdd63afa7529eb1290f73d0250a67c9f1
yong.wang <yong.wang@ucarinc.com>
2020-04-11 15:20:31 +0800

    第56天 Go 面试题 指针 结构体

--- src/main.go
+++ src/main.go
@@ -2,40 +2,19 @@ package main
 
 import "fmt"
 
-//type N int
-//
-//func (n N) value() {
-//	n++
-//	fmt.Printf("v:%p,%v\n", &n, n)
-//}
-//func (n *N) pointer() {
-//	*n++
-//	fmt.Printf("v:%p,%v\n", n, *n)
-//
-//}
-//func main() {
-//	var a N = 25
-//	p := &a
-//	p1 := &p
-//	(*p1).value()
-//	(*p1).pointer()
-//}
-
-type N int
-
-func (n N) test() {
-	fmt.Println(n)
+type T struct {
+	n int
 }
 
+func (t *T) Set(n int) {
+	t.n = n
+}
+func getT() T {
+	return T{}
+}
 func main() {
-	var n N = 10
-	fmt.Println(n) // 10
-
-	n++ //11
-	f1 := N.test
-	f1(n) // 11
-
-	n++ // 12
-	f2 := (*N).test
-	f2(&n) //12
+	//getT().Set(1)
+	t := getT()
+	t.Set(2)
+	fmt.Println(t.n)
 }
