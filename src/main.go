c8abc416179d1e8c6dc32a5d4d1ea46470de1d9c
yong.wang <yong.wang@ucarinc.com>
2020-04-11 17:59:35 +0800

    第39天 Go 面试题 结构体

--- src/main.go
+++ src/main.go
@@ -1,46 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func main1() {
-	ch := make(chan int, 100)
-	// A
-	go func() {
-		for i := 0; i < 10; i++ {
-			ch <- i
-		}
-		close(ch)
-	}()
-	// B
-	go func() {
-		for {
-			a, ok := <-ch
-			if !ok {
-				fmt.Println("close")
-				return
-			}
-			fmt.Println("a: ", a)
-		}
-	}()
-	//close(ch)
-	fmt.Println("ok")
-	time.Sleep(time.Second * 10)
+type User struct{}
+type User1 User
+type User2 = User
+
+func (i User1) m1() {
+	fmt.Println("m1")
+}
+func (i User) m2() {
+	fmt.Println("m2")
 }
 
-func Foo(x interface{}) {
-	if x == nil {
-		fmt.Println("empty interface")
-		return
-	}
-	fmt.Println("non-empty interface")
-}
 func main() {
-	var x *int = nil
-	Foo(x)
+	var i1 User1
+	var i2 User2
+	i1.m1()
+	i2.m2()
 }
 
-// 执行结果 non-empty interface
-// 考点：interface 的内部结构，我们知道接口除了有静态类型，还有动态类型和动态值，当且仅当动态值和动态类型都为 nil 时，接口类型值才为 nil。这里的 x 的动态类型是 *int，所以 x 不为 nil。
+// 执行结果
+//m1
+//m2
