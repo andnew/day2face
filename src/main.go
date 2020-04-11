0d493559c76c13711584daaec7d2ff543a74a64b
yong.wang <yong.wang@ucarinc.com>
2020-04-11 15:13:03 +0800

    第56天 Go 面试题 指针 方法表达式

--- src/main.go
+++ src/main.go
@@ -1,13 +1,41 @@
 package main
 
-type T struct {
-	n int
-}
+import "fmt"
 
-func getT() *T {
-	return &T{}
+//type N int
+//
+//func (n N) value() {
+//	n++
+//	fmt.Printf("v:%p,%v\n", &n, n)
+//}
+//func (n *N) pointer() {
+//	*n++
+//	fmt.Printf("v:%p,%v\n", n, *n)
+//
+//}
+//func main() {
+//	var a N = 25
+//	p := &a
+//	p1 := &p
+//	(*p1).value()
+//	(*p1).pointer()
+//}
+
+type N int
+
+func (n N) test() {
+	fmt.Println(n)
 }
 
 func main() {
-	getT().n = 1
+	var n N = 10
+	fmt.Println(n) // 10
+
+	n++ //11
+	f1 := N.test
+	f1(n) // 11
+
+	n++ // 12
+	f2 := (*N).test
+	f2(&n) //12
 }
