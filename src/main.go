afa2d0d65f7c26c64b780dda646a9ae55cd1c5cf
yong.wang <yong.wang@ucarinc.com>
2020-04-12 07:23:14 +0800

    第31天 Go 面试题 defer pain 函数

--- src/main.go
+++ src/main.go
@@ -1,43 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
-func change(s ...int) {
-	fmt.Println("len = ", len(s), " , cap ", cap(s), " s = ", s)
-	s = append(s, 3)
-	fmt.Println("len = ", len(s), " , cap ", cap(s), " s = ", s)
+func f(n int) (r int) {
+	defer func() {
+		r += n
+		if err := recover(); err != nil {
+			fmt.Println("9999")
+			debug.PrintStack()
+		}
+	}()
+
+	var f func()
+
+	//defer f() // 未定义，引发异常
+	f = func() {
+		fmt.Println("前======", r)
+		r += 2
+		fmt.Println("后======", r)
+	}
+	defer f()
+	return n + 1
 }
-
-func main1() {
-	slice := make([]int, 5, 5)
-	slice[0] = 1
-	slice[1] = 2
-	change(slice...)
-	fmt.Println(slice)
-	change(slice[0:2]...)
-	fmt.Println(slice)
-}
-
-// 总结&分析
-// 执行结果
-// len =  5  , cap  5  s =  [1 2 0 0 0]
-// len =  6  , cap  10  s =  [1 2 0 0 0 3]
-// [1 2 0 0 0]
-// len =  2  , cap  5  s =  [1 2]
-// len =  3  , cap  5  s =  [1 2 3]
-// [1 2 3 0 0]
 
 func main() {
-	var a = []int{1, 2, 3, 4, 5}
-	var r [5]int
-
-	for i, v := range a {
-		if i == 0 {
-			a[1] = 12 // 1，12，3, 4, 5
-			a[2] = 13 // 1，12，13, 4, 5
-		}
-		r[i] = v // 1, 12, 13, 4, 5
-	}
-	fmt.Println("r = ", r)
-	fmt.Println("a = ", a)
+	fmt.Println(f(3))
 }
