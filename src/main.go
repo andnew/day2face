d268af2411ae71ceaa165dcb73bfdfa76af105fd
yong.wang <yong.wang@ucarinc.com>
2020-04-11 17:35:04 +0800

    第43天 Go 面试题 变量 切片 内置函数

--- src/main.go
+++ src/main.go
@@ -1,13 +1,48 @@
 package main
 
-func main() {
+import "fmt"
 
-	var s []int
-	s = append(s, 1)
+var gvar int
 
-	var m map[string]int
-	m = make(map[string]int)
-	m["one"] = 1
+func main1() {
+	var one int
+	two := 2
+	var three int
+	three = 3
+
+	func(unused string) {
+		fmt.Println("Unused arg. No compile error")
+	}("what?")
+
+	fmt.Println(one, two, three)
 }
 
-// 执行结果 panic: assignment to entry in nil map
+// 变量声明与使用
+
+//type ConfigOne struct {
+//	Daemon string
+//}
+//
+//func (c *ConfigOne) String() string {
+//	return fmt.Sprintf("print: %v", c)
+//}
+//
+//func main() {
+//	c := &ConfigOne{}
+//	c.String()
+//}
+
+func main() {
+	var a = []int{1, 2, 3, 4, 5}
+	var r = make([]int, 0)
+
+	for i, v := range a {
+		if i == 0 {
+			a = append(a, 6, 7)
+		}
+
+		r = append(r, v)
+	}
+
+	fmt.Println(r, a)
+}
