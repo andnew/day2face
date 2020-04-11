aa205fded7fc750f46cb4106d24d31f9c5c6cf03
yong.wang <yong.wang@ucarinc.com>
2020-04-11 17:08:44 +0800

    第46天 Go 面试题 代码多行写 byte uint8 rune

--- src/main.go
+++ src/main.go
@@ -1,33 +1,22 @@
 package main
 
-//func main() {
-//	const x = 123
-//	const y = 1.23
-//	fmt.Println(x)
-//}
-// 总结&分析
-// 编译可以通过。知识点：常量。常量是一个简单值的标识符，在程序运行时，不会被修改的量。不像变量，常量未使用是能编译通过的。
+import "fmt"
 
-//const (
-//	x uint16 = 120
-//	y
-//	y1
-//	s = "abc"
-//	z
-//)
-//
 //func main() {
-//	fmt.Printf("%T %v\n", y, y)
-//	fmt.Printf("%T %v\n", z, z)
+//	x := []int{
+//		1,
+//		2,
+//	}
+//	_ = x
 //}
-// 总结&分析
-//uint16 120
-//string abc
 
+func test(x byte) {
+	fmt.Println(x)
+}
 func main() {
-	//var x string = nil
-	//
-	//if x == nil {
-	//	x = "default"
-	//}
+	var a byte = 0x11
+	var b uint8 = a
+	var c uint8 = a + b
+	test(c)
+	fmt.Println(a, b)
 }
