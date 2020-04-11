de1e05e62032e3ffae8ca28d21e1e277c99042b7
yong.wang <yong.wang@ucarinc.com>
2020-04-11 15:40:36 +0800

    第55天 Go 面试题 map

--- src/main.go
+++ src/main.go
@@ -2,19 +2,31 @@ package main
 
 import "fmt"
 
-type T struct {
-	n int
+type X struct{}
+
+func (x *X) test() {
+	println(x)
 }
 
-func (t *T) Set(n int) {
-	t.n = n
+func main1() {
+	var a *X
+	a.test()
+
+	(&X{}).test()
 }
-func getT() T {
-	return T{}
+func main2() {
+	var a *X
+	a.test() // 相当于 test(nil)
+
+	var x = X{}
+	x.test()
 }
+
 func main() {
-	//getT().Set(1)
-	t := getT()
-	t.Set(2)
-	fmt.Println(t.n)
+	x := map[string]string{"one": "a", "two": "", "three": "c"}
+
+	//if v := x["two"]; v == "" {
+	if _, ok := x["two"]; !ok {
+		fmt.Println("no entry")
+	}
 }
