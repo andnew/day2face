14140cd59afc6e83792b54955a3f61a3d0bfb1b0
yong.wang <yong.wang@ucarinc.com>
2020-04-11 17:01:32 +0800

    第47天 Go 面试题 常量 字符串

--- src/main.go
+++ src/main.go
@@ -1,18 +1,33 @@
 package main
 
-import "fmt"
+//func main() {
+//	const x = 123
+//	const y = 1.23
+//	fmt.Println(x)
+//}
+// 总结&分析
+// 编译可以通过。知识点：常量。常量是一个简单值的标识符，在程序运行时，不会被修改的量。不像变量，常量未使用是能编译通过的。
+
+//const (
+//	x uint16 = 120
+//	y
+//	y1
+//	s = "abc"
+//	z
+//)
+//
+//func main() {
+//	fmt.Printf("%T %v\n", y, y)
+//	fmt.Printf("%T %v\n", z, z)
+//}
+// 总结&分析
+//uint16 120
+//string abc
 
 func main() {
-	x := 1
-	fmt.Println(x) // 1
-	{
-		fmt.Println(x)    // 1
-		i, x := 2, 2      // i = 2 , x = 2
-		fmt.Println(i, x) // 2 , 2
-	}
-	fmt.Println(x) // print ?  1
-}
+	var x string = nil
 
-// 总结&分析
-// 变量隐藏。使用变量简短声明符号 := 时，如果符号左边有多个变量，只需要保证至少有一个变量是新声明的，并对已定义的变量尽进行赋值操作。
-// 但如果出现作用域之后，就会导致变量隐藏的问题
+	if x == nil {
+		x = "default"
+	}
+}
