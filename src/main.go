640d254e25df4018c611a34c319b707aaaa616fa
yong.wang <yong.wang@ucarinc.com>
2020-04-11 14:54:16 +0800

    第57天 Go 面试题 切片

--- src/main.go
+++ src/main.go
@@ -3,35 +3,30 @@ package main
 import "fmt"
 
 func main1() {
-	var x interface{}
-	var y interface{} = []int{3, 5}
-	_ = x == x
-	_ = x == y
-	_ = y == y // 发生panic
+	s := make([]int, 3, 9)
+	fmt.Println(len(s)) //3
+	s2 := s[4:8]
+	fmt.Println(len(s2)) //4
 }
 
 // 总结&分析
-// 因为两个比较值的动态类型为同一个不可比较类型。
+// 执行结果是 3 4
 
-var o = fmt.Print
+type N int
+
+func (n N) test() {
+	fmt.Println(n)
+}
 
 func main() {
-	c := make(chan int, 1)
-	for range [3]struct{}{} {
-		select {
-		default:
-			o(1)
-		case <-c:
-			o(2)
-			c = nil
-		case c <- 1:
-			o(3)
-		}
-	}
+	var n N = 10
+	p := &n
+	n++
+	f1 := n.test
+	n++
+	f2 := p.test
+	n++
+	fmt.Println(n)
+	f1()
+	f2()
 }
-
-// 总结&分析
-// 第一次循环，写操作已经准备好，执行 o(3)，输出 3；
-// 第二次，读操作准备好，执行 o(2)，输出 2 并将 c 赋值为 nil；
-// 第三次，由于 c 为 nil，走的是 default 分支，输出 1。
-// 执行结果是 321
