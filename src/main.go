60a00c056b739fb5d89ccaecc248a9c16a008e7c
yong.wang <yong.wang@ucarinc.com>
2020-04-11 14:32:14 +0800

    第60天 Go 面试题 指针

--- src/main.go
+++ src/main.go
@@ -1,97 +1,41 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-//type T struct{}
-//
-//func (*T) foo() {
-//}
-//func (T) bar() {
-//}
-//
-//type S struct {
-//	*T
-//}
-//
-//func main() {
-//	s := S{}
-//	fmt.Printf("%#v\n",s) //main.S{T:(*main.T)(nil)}
-//	_ = s.foo
-//	s.foo()
-//	_ = s.bar //s.bar 将被展开为 (*s.T).bar
-//}
-// 总结&分析
-// panic: runtime error: invalid memory address or nil pointer dereference
+type N int
 
-//type data struct {
-//	sync.Mutex
-//}
-//
-//func (d data) test(s string) {// 修改为 func (d *data) test(s string) {
-//	d.Lock()
-//	defer d.Unlock()
-//
-//	for i := 0; i < 5; i++ {
-//		fmt.Println(s, i)
-//		time.Sleep(time.Second)
-//	}
-//}
-//
-//func main() {
-//
-//	var wg sync.WaitGroup
-//	wg.Add(2)
-//	var d data
-//
-//	go func() {
-//		defer wg.Done()
-//		d.test("read")
-//	}()
-//
-//	go func() {
-//		defer wg.Done()
-//		d.test("write")
-//	}()
-//
-//	wg.Wait()
-//}
-// 总结&分析
-// 锁失效。将 Mutex 作为匿名字段时，相关的方法必须使用指针接收者，否则会导致锁机制失效。
-
-type data struct {
-	*sync.Mutex // *Mutex
+func (n *N) test() {
+	fmt.Println(*n)
 }
 
-func (d data) test(s string) { // 值方法
-	d.Lock()
-	defer d.Unlock()
+func main1() {
+	var n N = 10
+	p := &n
 
-	for i := 0; i < 5; i++ {
-		fmt.Println(s, i)
-		time.Sleep(time.Second)
-	}
+	n++ // 11
+	f1 := n.test
+
+	n++ //12
+	f2 := p.test
+
+	n++            //13
+	fmt.Println(n) //13
+
+	f1() //13
+	f2() //13
 }
+
+// 总结&分析
+// 方法值。当目标方法的接收者是指针类型时，那么被复制的就是指针。
 
 func main() {
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	d := data{new(sync.Mutex)} // 初始化
-
-	go func() {
-		defer wg.Done()
-		d.test("read")
-	}()
-
-	go func() {
-		defer wg.Done()
-		d.test("write")
-	}()
-
-	wg.Wait()
+	var m map[int]bool // nil
+	_ = m[123]
+	var p *[5]string // nil
+	for range p {
+		_ = len(p)
+	}
+	var s []int // nil
+	_ = s[:]
+	s, s[0] = []int{1, 2}, 9
 }
