740ccd5e493d8e142e74bf9712c006721fabf861
yong.wang <yong.wang@ucarinc.com>
2020-04-11 14:59:24 +0800

    第57天 Go 面试题 指针

--- src/main.go
+++ src/main.go
@@ -1,32 +1,13 @@
 package main
 
-import "fmt"
-
-func main1() {
-	s := make([]int, 3, 9)
-	fmt.Println(len(s)) //3
-	s2 := s[4:8]
-	fmt.Println(len(s2)) //4
+type T struct {
+	n int
 }
 
-// 总结&分析
-// 执行结果是 3 4
-
-type N int
-
-func (n N) test() {
-	fmt.Println(n)
+func getT() *T {
+	return &T{}
 }
 
 func main() {
-	var n N = 10
-	p := &n
-	n++
-	f1 := n.test
-	n++
-	f2 := p.test
-	n++
-	fmt.Println(n)
-	f1()
-	f2()
+	getT().n = 1
 }
