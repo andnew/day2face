2b3135a79a7fe15992de19c860338777dc26c3be
yong.wang <yong.wang@ucarinc.com>
2020-04-11 17:26:05 +0800

    第44天 Go 面试题 map make 初始化

--- src/main.go
+++ src/main.go
@@ -1,23 +1,13 @@
 package main
 
-import "fmt"
-
-type info struct {
-	result int
-}
-
-func work() (int, error) {
-	return 13, nil
-}
-
 func main() {
-	var data info
-	var err error
-	data.result, err = work()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
-	fmt.Printf("info: %+v\n", data)
+	var s []int
+	s = append(s, 1)
+
+	var m map[string]int
+	m = make(map[string]int)
+	m["one"] = 1
 }
+
+// 执行结果 panic: assignment to entry in nil map
