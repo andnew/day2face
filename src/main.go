7d62dc32587d13a2f9cdf1f8ae7f5d368e6341a4
yong.wang <yong.wang@ucarinc.com>
2020-04-11 14:00:37 +0800

    第62天 Go 面试题 for-range

--- src/main.go
+++ src/main.go
@@ -2,23 +2,35 @@ package main
 
 import "fmt"
 
-func F(n int) func() int {
-	return func() int {
-		n++
-		return n
-	}
-}
-
-func main() {
-	f := F(5)
-	defer func() {
-		fmt.Println(f())
-	}()
-	defer fmt.Println(f())
-	i := f()
-	fmt.Println(i)
-}
+//func main() {
+//	var k = 1
+//	var s = []int{1, 2}
+//	k, s[k] = 0, 3
+//	fmt.Println(s[0] + s[1])
+//}
 
 // 总结&分析
-// 执行结果 7 6 8
-// 知识点，defer 执行顺序 ，先进后出， 值传递 和 匿名函数
+// k, s[k] = 0, 3 ==> k, s[1] = 0, 3
+
+func main() {
+	var k = 9
+	for k = range []int{} {
+		fmt.Println("=====")
+	}
+	fmt.Println(k)
+
+	for k = 0; k < 3; k++ {
+		fmt.Println("----")
+	}
+	fmt.Println(k)
+
+	for k = range (*[3]int)(nil) {
+		fmt.Println("666--")
+	}
+	fmt.Println(k)
+}
+
+// 执行结果
+//9
+//3
+//2
