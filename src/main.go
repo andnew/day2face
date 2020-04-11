632daac5eaad82ebd73400e009a243140af871f5
yong.wang <yong.wang@ucarinc.com>
2020-04-11 13:32:05 +0800

    第64天 Go 面试题 defer recover

--- src/main.go
+++ src/main.go
@@ -2,47 +2,34 @@ package main
 
 import "fmt"
 
-//type T struct {
-//	n int
-//}
-//
-//func main() {
-//	ts := [2]T{}
-//	for i, t := range ts  {
-//		switch i {
-//		case 0:
-//			t.n = 3
-//			ts[1].n = 9
-//		case 1:
-//			fmt.Print(t.n, " ")
-//		}
-//	}
-//	fmt.Print(ts)
-//}
-
-// 总结&分析
-// for-range 循环数组。此时使用的是数组 ts 的副本，所以 t.n = 3 的赋值操作不会影响原数组。
-// 执行结果
-// 0 [{0} {9}]
-
-type T struct {
-	n int
-}
-
 func main() {
-	ts := [2]T{}
-	for i, t := range &ts {
-		switch i {
-		case 0:
-			t.n = 3
-			ts[1].n = 9
-		case 1:
-			fmt.Print(t.n, " ")
-		}
-	}
-	fmt.Print(ts)
+	defer func() {
+		fmt.Print(recover()) // 对应的是 panic(1)
+	}()
+	defer func() {
+		defer fmt.Print(recover()) // 对应的是 panic(2)
+		panic(1)
+	}()
+	defer recover()
+	panic(2)
 }
 
+// 执行结果 21
+
+//func main() {
+//	defer func() {
+//		fmt.Print(recover())
+//	}()
+//	defer func() {
+//		defer func() {
+//			fmt.Print(recover())
+//		}()
+//		panic(1)
+//	}()
+//	panic(2)
+//}
+// 执行结果 12
+
 // 总结&分析
-// 知识点：for-range 数组指针。for-range 循环中的循环变量 t 是原数组元素的副本。如果数组元素是结构体值，则副本的字段和原数组字段是两个不同的值。
-// 参考答案及解析：9 [{0} {9}]。
+// 知识点：defer recover ,recover 必须放在defer 函数内
+// recover() 必须在 defer() 函数中调用才有效，所以第 9 行代码捕获是无效的。在调用 defer() 时，便会计算函数的参数并压入栈中，所以执行第 6 行代码时，此时便会捕获 panic(2)；此后的 panic(1)，会被上一层的 recover() 捕获。所以输出 21。
