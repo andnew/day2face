ce346f9c8af2d9cd386e775a0376a45461d479e5
yong.wang <yong.wang@ucarinc.com>
2020-04-12 21:26:55 +0800

    第14天 Go 面试题 指针

--- src/main.go
+++ src/main.go
@@ -2,34 +2,13 @@ package main
 
 import "fmt"
 
-func hello(i int) {
-	fmt.Println(i)
-}
-func main1() {
-	i := 5
-	defer hello(i)
-	i = i + 10
-}
-
-type People struct{}
-
-func (p *People) ShowA() {
-	fmt.Println("showA")
-	p.ShowB()
-}
-func (p *People) ShowB() {
-	fmt.Println("showB")
-}
-
-type Teacher struct {
-	People
-}
-
-func (t *Teacher) ShowB() {
-	fmt.Println("teacher showB")
+func incr(p *int) int {
+	*p++
+	return *p
 }
 
 func main() {
-	t := Teacher{}
-	t.ShowA()
+	p := 1
+	incr(&p)
+	fmt.Println(p)
 }
