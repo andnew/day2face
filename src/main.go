a6b9768fb177b654f4f5276f2bc06bc04b3c03dc
yong.wang <yong.wang@ucarinc.com>
2020-04-12 22:04:08 +0800

    第18天 Go 面试题 defer 返回参数是匿名与具名

--- src/main.go
+++ src/main.go
@@ -2,76 +2,32 @@ package main
 
 import "fmt"
 
-//type A interface {
-//	ShowA() int
-//}
-//
-//type B interface {
-//	ShowB() int
-//}
-//
-//type Work struct {
-//	i int
-//}
-//
-//func (w Work) ShowA() int {
-//	return w.i + 10
-//}
-//
-//func (w Work) ShowB() int {
-//	return w.i + 20
-//}
-//
-//func main1() {
-//	c := Work{3}
-//	var a A = c
-//	var b B = c
-//	fmt.Println(a.ShowA())
-//	fmt.Println(b.ShowB())
-//}
-type A interface {
-	ShowA() int
-}
+//f1()、f2()、f3() 函数分别返回什么？
 
-type B interface {
-	ShowB() int
-}
-
-type Work struct {
-	i int
-}
-
-func (w Work) ShowA() int {
-	return w.i + 10
-}
-
-func (w Work) ShowB() int {
-	return w.i + 20
-}
-
-func main2() {
-	var a A = Work{3}
-	s := a.(Work)
-	fmt.Println(s.ShowA())
-	fmt.Println(s.ShowB())
-}
-
-func increaseA() int {
-	var i int
-	defer func() {
-		i++
-	}()
-	return i
-}
-
-func increaseB() (r int) {
+func f1() (r int) {
 	defer func() {
 		r++
 	}()
-	return r
+	return 0
+}
+
+func f2() (r int) {
+	t := 5
+	defer func() {
+		t = t + 5
+	}()
+	return t
+}
+
+func f3() (r int) {
+	defer func(r int) {
+		r = r + 5
+	}(r)
+	return 1
 }
 
 func main() {
-	fmt.Println(increaseA())
-	fmt.Println(increaseB())
+	fmt.Println("f1() = ", f1())
+	fmt.Println("f2() = ", f2())
+	fmt.Println("f3() = ", f3())
 }
