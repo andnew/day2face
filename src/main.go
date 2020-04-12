c1dddf53f0c4af6c219c77becd1fd0423b35b597
yong.wang <yong.wang@ucarinc.com>
2020-04-12 23:02:06 +0800

    第107天 Go 面试题 for 坑

--- src/main.go
+++ src/main.go
@@ -1,37 +1,40 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	age int
-}
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	person := &Person{28}
+	var m = map[int]int{1: 1, 2: 2, 3: 3}
+	//only del key once, and not del the current iteration key
+	var o sync.Once
+	for i := range m {
+		o.Do(func() {
+			for _, key := range []int{1, 2, 3} {
+				if key != i {
+					fmt.Printf("when iteration key %d, del key %d\n", i, key)
+					delete(m, key)
+					break
+				}
+			}
+		})
+		fmt.Printf("%d-->%d ", i, m[i])
+	}
 
-	// 1.
-	defer fmt.Println("1  ,  ", person.age) // 28
-
-	// 2.
-	defer func(p *Person) {
-		fmt.Println("2  ,  ", p.age) // 29
-	}(person)
-
-	// 2.1
-	defer func(p Person) {
-		fmt.Println("2.1,  ", p.age) // 28
-	}(*person)
-
-	// 3.
-	defer func() {
-		fmt.Println("3  ,  ", person.age) // 29
-	}()
-
-	person.age = 29
+	fmt.Println("-------0x06 对 map 遍历时新增元素能遍历到么？--------")
+	var createElemDuringIterMap = func() {
+		var m = map[int]int{1: 1, 2: 2, 3: 3}
+		for i := range m {
+			m[4] = 4
+			fmt.Printf("%d===%d ", i, m[i])
+		}
+	}
+	for i := 0; i < 50; i++ {
+		//some line will not show 44, some line will
+		createElemDuringIterMap()
+		fmt.Println()
+	}
 }
 
 // 执行结果
-// 3  ,   29
-// 2.1,   28
-// 2  ,   29
-// 1  ,   28
