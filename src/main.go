50c14f52cdb44c3a37ee6e5301cacad5e8389e88
yong.wang <yong.wang@ucarinc.com>
2020-04-12 19:53:18 +0800

    第5天 Go 面试题 结构体 比较

--- src/main.go
+++ src/main.go
@@ -2,30 +2,34 @@ package main
 
 import "fmt"
 
-func main1() {
-	//list := new([]int)
-	// 正确的代码
-	list := make([]int, 0)
-	list = append(list, 1)
-	fmt.Println(list)
-}
-
-// 编译报错 Cannot use 'list' (type *[]int) as type []Type
-
-func main2() {
-	s1 := []int{1, 2, 3}
-	s2 := []int{4, 5}
-	s1 = append(s1, s2...)
-	fmt.Println(s1)
-}
-
-// 编译报错  Cannot use 's2' (type []int) as type int
-
-var (
-	size     = 1024
-	max_size = size * 2
-)
-
 func main() {
-	fmt.Println(size, max_size)
+	sn1 := struct {
+		age  int
+		name string
+	}{age: 11, name: "qq"}
+
+	sn2 := struct {
+		age  int
+		name string
+	}{age: 11, name: "qq"}
+
+	if sn1 == sn2 {
+		fmt.Println("sn1 == sn2") // true
+	}
+	sm1 := struct {
+		age int
+		m   map[string]string
+	}{age: 11, m: map[string]string{"a": "1"}}
+
+	sm2 := struct {
+		age int
+		m   map[string]string
+	}{age: 11, m: map[string]string{"a": "1"}}
+
+	_ = sm1
+	_ = sm2
+
+	//if sm1 == sm2 {
+	//	fmt.Println("sm1 == sm2")
+	//}
 }
