7d915eade9a73a1e0233b592fbe1a7418f462307
yong.wang <yong.wang@ucarinc.com>
2020-04-12 19:59:22 +0800

    第5天 Go 面试题 类型

--- src/main.go
+++ src/main.go
@@ -2,34 +2,14 @@ package main
 
 import "fmt"
 
+type MyInt1 int
+type MyInt2 = int
+
 func main() {
-	sn1 := struct {
-		age  int
-		name string
-	}{age: 11, name: "qq"}
-
-	sn2 := struct {
-		age  int
-		name string
-	}{age: 11, name: "qq"}
-
-	if sn1 == sn2 {
-		fmt.Println("sn1 == sn2") // true
-	}
-	sm1 := struct {
-		age int
-		m   map[string]string
-	}{age: 11, m: map[string]string{"a": "1"}}
-
-	sm2 := struct {
-		age int
-		m   map[string]string
-	}{age: 11, m: map[string]string{"a": "1"}}
-
-	_ = sm1
-	_ = sm2
-
-	//if sm1 == sm2 {
-	//	fmt.Println("sm1 == sm2")
-	//}
+	var i int = 0
+	var i1 MyInt1 = i // 修改代码 MyInt1(i)
+	var i2 MyInt2 = i
+	fmt.Println(i1, i2)
 }
+
+// 编译报错 Cannot use 'i' (type int) as type MyInt1 in assignment
